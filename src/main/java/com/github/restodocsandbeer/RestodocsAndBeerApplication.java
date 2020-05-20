b737741f474c0dba281c358691fffbefaba17137
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-05-20 13:07:30 +0200

    Init version

---
+++ src/main/java/com/github/restodocsandbeer/RestodocsAndBeerApplication.java
@@ -0,0 +1,13 @@
+package com.github.restodocsandbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestodocsAndBeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestodocsAndBeerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/restodocsandbeer/RestodocsAndBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.restodocsandbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestodocsAndBeerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
