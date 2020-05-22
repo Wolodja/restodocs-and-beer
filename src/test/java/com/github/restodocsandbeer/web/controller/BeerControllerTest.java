21ccae92a9520dfa05ce4ae1fa2bd2122726bad0
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-05-22 15:21:15 +0200

    Document generation enhanced

--- src/test/java/com/github/restodocsandbeer/web/controller/BeerControllerTest.java
+++ src/test/java/com/github/restodocsandbeer/web/controller/BeerControllerTest.java
@@ -62,7 +62,7 @@ class BeerControllerTest {
                 .param("iscold", "yes")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-get",
                         pathParameters(
                                 parameterWithName("beerId").description("UUID of desired beer to get.")
                         ),
@@ -94,7 +94,7 @@ class BeerControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(status().isCreated())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-new",
                         requestFields(
                                 fields.withPath("id").ignored(),
                                 fields.withPath("version").ignored(),
